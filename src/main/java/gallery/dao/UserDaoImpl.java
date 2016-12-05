47984855243c40dd7077b41db0fd8f7d8b6ae875
Albert Roschier <a.e.roschier@hotmail.com>
2016-12-05 13:39:54 +0200

    Update

--- src/main/java/gallery/dao/UserDaoImpl.java
+++ src/main/java/gallery/dao/UserDaoImpl.java
@@ -2,23 +2,31 @@ package gallery.dao;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import gallery.model.User;
 
 @Repository
 public class UserDaoImpl implements UserDao {
+	
+	private JdbcTemplate jdbcTemplate;
 
+	@Autowired
+	public void setDataSource(DataSource datasource) {
+		jdbcTemplate = new JdbcTemplate(datasource);
+	}
+	
 	@Override
-	public User getUser(int userid) {
-		return null;
+	public User getUser(int username) {
+		return jdbcTemplate.queryForObject("SELECT username, first_name, last_name FROM user WHERE username = ?", new Object[] {username}, new UserRowMapper());
 	}
 
 	@Override
 	public List<User> listUser() {
-		return null;
+		return jdbcTemplate.query("SELECT username, first_name, last_name FROM user", new UserRowMapper());
 	}
-
-	
 
 }
